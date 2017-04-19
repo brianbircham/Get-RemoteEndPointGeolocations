f89d191090b4efeb4efcbee579717cf0eab81ffb
Brian Bircham <brian.bircham@outlook.com>
2017-04-19 18:55:04 +0200

    Reshuffle to bring back ordering and replaced IP address with domain name for geolocation requests

--- Get-RemoteEndPointGeolocations.ps1
+++ Get-RemoteEndPointGeolocations.ps1
@@ -6,25 +6,27 @@
     try {
         $Connections = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties().GetActiveTcpConnections()
         foreach($Connection in $Connections) {
-            $Hash = [ordered]@{
-                RemoteCountry = $null
-                RemoteCity    = $null
-            }
-            
+            $Hash = [ordered]@{}
             $Hash.LocalAddress  = $Connection.LocalEndPoint.Address
             $Hash.LocalPort     = $Connection.LocalEndPoint.Port
             $Hash.RemoteAddress = $Connection.RemoteEndPoint.Address
             $Hash.RemotePort    = $Connection.RemoteEndPoint.Port
-            $Hash.State         = $Connection.State
-            $Hash.IPV4Or6       = if($Connection.LocalEndPoint.AddressFamily -eq "InterNetwork" ) { "IPv4" } else { "IPv6" }
 
             # Only send non-RFC-1918 addresses for Geo-location
             if($Output.RemoteAddress -notmatch "(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)|(^127\.0\.0\.1)") {
                 $remoteIp = $Connection.RemoteEndPoint.Address
-                $response = (Invoke-WebRequest "http://158.69.242.138/json/$remoteIp").Content | ConvertFrom-Json
+                $response = (Invoke-WebRequest "http://www.freegeoip.net/json/$remoteIp").Content | ConvertFrom-Json
                 $Hash.RemoteCountry = $response."country_name"
                 $Hash.RemoteCity    = $response."city"
+            } else {
+                $Hash.RemoteCountry = $null
+                $Hash.RemoteCity    = $null
             }
+
+            $Hash.State         = $Connection.State
+            $Hash.IPV4Or6       = if($Connection.LocalEndPoint.AddressFamily -eq "InterNetwork" ) { "IPv4" } else { "IPv6" }
+
+
 
             [pscustomobject]$Hash
         }
@@ -34,4 +36,4 @@
     }
 }
 
-Get-RemoteEndPointGeolocations | Format-Table
+Get-RemoteEndPointGeolocations | Format-Table 
